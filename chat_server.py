74cfd2e2e9eef29311b7a80cc19d9be51a5cbb91
jumper casa <codefacs@gmail.com>
2020-10-06 19:26:19 +0200

    il primo...

---
+++ chat_server.py
@@ -0,0 +1,75 @@
+
+
+
+import socket
+import threading
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(("192.168.1.229", 6000))
+
+
+list_of_sockets = []
+address_list = []
+server_socket.listen()
+server_socket.setblocking(False)
+print("server is running!!!")
+
+
+def add_socket():
+    global list_of_sockets
+    while True:
+        # print("while loop...")
+        server_socket.setblocking(False)
+        try:
+            client, addr = server_socket.accept()
+
+            list_of_sockets.append(client)
+            address_list.append(addr)
+            print("connection from", addr)
+        except BlockingIOError:
+            continue
+
+
+def read_socket():
+    global list_of_sockets
+    while True:
+
+        for i in range(len(list_of_sockets)):
+            try:
+                list_of_sockets[i].setblocking(False)
+                try:
+                    request = list_of_sockets[i].recv(1024)
+
+                    if not request:
+                        list_of_sockets[i].close()
+
+                    else:
+                        for k in range(len(list_of_sockets)):
+                            if list_of_sockets[i] == list_of_sockets[k]:
+                                pass
+                            else:
+                                request = f"{address_list[k]}{request.decode()}".encode()
+                                list_of_sockets[k].sendall(request)
+
+                        print("number of connection = ", len(list_of_sockets))
+                        # list_of_sockets[i].close()
+                        continue
+                except BlockingIOError:
+                    continue
+
+            except OSError:
+                list_of_sockets.remove(list_of_sockets[i])
+                break
+
+
+t1 = threading.Thread(target=add_socket)
+t2 = threading.Thread(target=read_socket)
+t1.start()
+t2.start()
+
+t1.join()
+
+t2.join()
+
+
